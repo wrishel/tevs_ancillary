a13b915ecf740d946ba837dd8ffc415819ab2f9b
Wes Rishel <wrishel@gmail.com>
2017-09-05 18:48:14 -0700

    working on 2016-11

--- b/import_from_tevs_2.sql
+++ b/import_from_tevs_2.sql
@@ -55,11 +55,32 @@ insert into voteops
 	select * from pmitch.voteops
 ;
 
-select distinct choice_text from voteops;
-select distinct contest_text from voteops;
+COPY (select distinct choice_text from voteops)
+TO '/Users/Wes/NotForTheCloud/choices.csv' WITH csv ;
+COPY (select distinct contest_text from voteops)
+TO '/Users/Wes/NotForTheCloud/contests.csv' WITH csv ;
+
+
 truncate table ocrc_choice;
 truncate table ocrc_contest;
 truncate table harts;
 
+/*
+   1) Run octransforms.py on choices.csv
+   2) edit the output to create acceptable values for each erroneous value
+        (EDITING WITH EXCEL WILL NOT WORK BECAUSE IT GENERATES #NAME VALUES NO MATTER WHAT)
+        include "to be fixed in the voteop_good field for values that can't be determined
+
+   3) import into ocrc_choice
+   4) get template and image file names of choices unfixed and create corrections.
 
 
+   5) repeat steps 1-4 for contests.csv, input to ocrc_contest.
+ */
+
+-- COPY ocrc_choice (voteop_good, voteop_other) FROM '/Users/Wes/NotForTheCloud/OCRC_choices.csv' with csv
+SELECT max(voteop_good), voteop_other, min(file1),max(code_string)
+  FROM ocrc_choice o, voteops v, ballots b
+  WHERE voteop_good LIKE '%to be fixed%'
+  AND o.voteop_other = v.choice_text and v.ballot_id = b.ballot_id
+  GROUP BY voteop_other
